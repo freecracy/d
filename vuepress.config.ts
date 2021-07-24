faa918d917534a62dc04bb4cd7a910d0aea651f1
freecracy <12114073+freecracy@users.noreply.github.com>
2021-07-24 14:30:54 +0800

    init

---
+++ vuepress.config.ts
@@ -0,0 +1,39 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import { readdirSync } from "fs";
+
+let navbar = [
+	{
+		text: "联系我",
+		link: "http://wpa.qq.com/msgrd?v=3&uin=295984047&site=qq&menu=yes",
+	},
+];
+
+let sidebar = list("./docs/").reverse();
+
+export default defineUserConfig<DefaultThemeOptions>({
+	base: "/d/",
+	lang: "zh-CN",
+	title: "daily",
+	description: "just a little",
+	themeConfig: {
+		logo: "https://vuejs.org/images/logo.png",
+		navbar,
+		sidebar,
+		lastUpdatedText: "最近更新",
+		contributors: false,
+	},
+	bundler: "@vuepress/vite",
+});
+
+function list(p: string): string[] {
+	let s: string[] = [];
+	const files = readdirSync(p);
+	for (const file of files) {
+		if (file.endsWith(".md")) {
+			s.push("/" + file.toString());
+		}
+	}
+	console.log(s);
+	return s;
+}
